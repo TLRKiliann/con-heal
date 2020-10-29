b2576e4423d39e7874084f2fe5d0687471c094f2
TLRKiliann <philogenie@protonmail.com>
2020-10-29 23:55:43 +0100

    file update for file txt and db created

--- update/updatepatient1.py
+++ update/updatepatient1.py
@@ -17,7 +17,97 @@ gui=Tk()
 gui.title("Enter new patient")
 gui.configure(bg='cyan')
 
+def uptopat(idpatient, patient_num, firstpat, firstname_pat,
+    surname, sur_pat, birthvalue, birth_entree, allergia, allergy_pat,
+    transdisval, diseasetrans, diagnosis, diagnos_pat):
+    """
+        Update data for patients
+        in function of their id
+    """
 
+    idpatient = patient_num.get()
+    firstpat = firstname_pat.get()
+    surname = sur_pat.get()
+    birthvalue = birth_entree.get()
+    allergia = allergy_pat.get()
+    transdisval = diseasetrans.get()
+    diagnosis = diagnos_pat.get()
+
+
+    if patientnum.get() == "" or labelfirst.get() == "" or lblsurname.get() == "":
+        messagebox.showerror("MySQL Connection", "Enter Correct Details.")
+    else:
+        sqlCon = pymysql.connect(host='127.0.0.1', user='root', password='Ko@l@tr3379', database='timetrackconn')
+        cur = sqlCon.cursor()
+        cur.execute("INSERT into timetrackconn values (%s, %s, %s, %s, %s, %s, %s)",(
+        patient_num.get(),
+        firstname_pat.get(),
+        sur_pat.get(),
+        birth_entree.get(),
+        allergy_pat.get(),
+        diseasetrans.get(),
+        diagnos_pat.get(),
+        ))
+        sqlCon.commit()
+        sqlCon.close()
+        messagebox.showinfo("MySQL connection", "Record Entered Successfully !")
+    
+    if idpatient == 1:
+        try:
+            if os.path.getsize('./newpatient/entryfile.txt'):
+                print("+ File 'entryfile.txt' exist !")
+                os.remove('./newpatient/entryfile.txt')
+                searchLineName(firstpat, surname, birthvalue, allergia, transdisval, diagnosis)
+        except FileNotFoundError as outcom1:
+            print("+ Sorry, file 'entryfile.txt' not exist !")
+            print(str(outcom1))
+            messagebox.showwarning("Warning", "File entryfile.txt not found !")
+    else:
+        pass
+
+    if idpatient == 2:
+        try:
+            if os.path.getsize('./newpatient/entryfile2.txt'):
+                print("+ File 'entryfile2.txt' exist !")
+                os.remove('./newpatient/entryfile2.txt')
+                searchLineName2(firstpat, surname, birthvalue, allergia, transdisval, diagnosis)
+        except FileNotFoundError as outcom1:
+            print("+ Sorry, file 'entryfile2.txt' not exist !")
+            print(str(outcom1))
+            messagebox.showwarning("Warning", "File entryfile2.txt not found !")
+    else:
+        pass
+
+    gui.destroy()
+
+def searchLineName(firstpat, surname, birthvalue, allergia, transdisval, diagnosis):
+    """
+        To save changing data for 
+        entryfile.txt and display
+        messagebox.
+    """
+    MsgBox = messagebox.askyesno('Save data', 'Do you want to save ?')
+    if MsgBox == 1:
+        with open('./newpatient/entryfile.txt', 'w') as file:
+            file.write(firstpat + " " + surname + '\n')
+            file.write(birthvalue + '\n')
+            file.write(allergia + '\n')
+            file.write(transDisVal + '\n')
+            file.write(diagnosis + '\n')
+            file.close()
+    messagebox.showinfo("Info", "Data was updated for entryfile.txt !")
+
+def searchLineName2(firstpat, surname, birthvalue, allergia, transdisval, diagnosis):
+    MsgBox2 = messagebox.askyesno('Save data', 'Do you want to save ?')
+    if MsgBox2 == 1:
+        with open('./newpatient/entryfile2.txt', 'w') as file2:
+            file2.write(firstpat + " " + surname + '\n')
+            file2.write(birthvalue + '\n')
+            file2.write(allergia + '\n')
+            file2.write(transDisVal + '\n')
+            file2.write(diagnosis + '\n')
+            file2.close()
+    messagebox.showinfo("Info", "Data was updated for entryfile2.txt !")
 
 labelID = Label(gui)
 labelID = Label(text='ID : ',
@@ -38,9 +128,9 @@ labelname = Label(text='Name : ',
     fg='RoyalBlue4', bg='cyan')
 labelname.pack(pady=10)
 
-patientname = StringVar()
-patientname.set('Firstname')
-firstname_pat = Entry(gui, textvariable=patientname,
+firstpat = StringVar()
+firstpat.set('Firstname')
+firstname_pat = Entry(gui, textvariable=firstpat,
     highlightbackground='light sky blue',
     bd=4)
 firstname_pat.pack()
@@ -105,8 +195,8 @@ diagnos_pat.pack()
 bouton1 = Button(gui, text="Enter", width=8, bd=3,
     fg='yellow', bg='RoyalBlue3', highlightbackground='light sky blue',
     activebackground='dark turquoise',
-    command = lambda: get(idpatient, patient_num, patientname, firstname_pat,
-        surname, sur_pat, allergia, allergy_pat, birthvalue, birth_entree, 
+    command = lambda: uptopat(idpatient, patient_num, firstpat, firstname_pat,
+        surname, sur_pat, birthvalue, birth_entree, allergia, allergy_pat,
         transdisval, diseasetrans, diagnosis, diagnos_pat))
 bouton1.pack(side=LEFT, padx=10, pady=20)
 

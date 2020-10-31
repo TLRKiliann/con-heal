4a92d906c1b8e2b836302510afe01695fd3b91e3
TLRKiliann <philogenie@protonmail.com>
2020-10-31 11:02:02 +0100

    nutrition files are ok

--- nutrition/nutrit_patient19.py
+++ nutrition/nutrit_patient19.py
@@ -2,8 +2,16 @@
 # -*- coding:utf-8 -*-
 
 
+"""
+    To record intolerance in allergy file.
+    It will appear in the text zone 'Allergy'
+    of app with text zone of name.
+"""
+
+
 from tkinter import *
 from tkinter import messagebox
+import subprocess
 
 
 gui = Tk()
@@ -11,6 +19,9 @@ gui.title("Intolerances")
 gui.configure(bg='RoyalBlue4')
 
 def saveCheck():
+    """
+        To ask if user want save them choices
+    """
     MSB = messagebox.askyesno('Save Data', 'Data saved !')
     if MSB == 1:
         print("Ok, data")
@@ -18,7 +29,7 @@ def saveCheck():
         confRec()
         gui.destroy()
     else:
-        NoforQ = messagebox.showinfo('Return', 'Data not saved')
+        messagebox.showinfo('Return', 'Data not saved')
 
 def recordOption():
     """
@@ -28,8 +39,7 @@ def recordOption():
     if CheckVar1.get()==1:
         print("Gluten intolerance")
         with open('./allergy/allergyfile19.txt', 'a+') as file:
-            file.write("Gluten intolerance\n")
-            file.write(str('----------------\n'))
+            file.write("Gluten, ")
     else:
         print("Nothing to do")
 
@@ -37,8 +47,7 @@ def recordOption():
     if CheckVar2.get()==1:
         print("Lactose intolerance")
         with open('./allergy/allergyfile19.txt', 'a+') as file:
-            file.write("Lactose intolerance\n")
-            file.write(str('----------------\n'))
+            file.write("Lactose, ")
     else:
         print("Nothing to do")
 
@@ -46,8 +55,7 @@ def recordOption():
     if CheckVar3.get()==1:
         print("Saccharose intolerance")
         with open('./allergy/allergyfile19.txt', 'a+') as file:
-            file.write("Saccharose intolerance\n")
-            file.write(str('----------------\n'))
+            file.write("Saccharose, ")
     else:
         print("Nothing to do")
 
@@ -55,8 +63,7 @@ def recordOption():
     if CheckVar4.get()==1:
         print("Fructose intolerance")
         with open('./allergy/allergyfile19.txt', 'a+') as file:
-            file.write("Fructose intolerance\n")
-            file.write(str('----------------\n'))
+            file.write("Fructose, ")
     else:
         print("Nothing to do")
 
@@ -64,8 +71,7 @@ def recordOption():
     if CheckVar5.get()==1:
         print("Eggs")
         with open('./allergy/allergyfile19.txt', 'a+') as file:
-            file.write("Eggs\n")
-            file.write(str('----------------\n'))
+            file.write("Eggs, ")
     else:
         print("Nothing to do")
 
@@ -73,8 +79,7 @@ def recordOption():
     if CheckVar6.get()==1:
         print("Fish")
         with open('./allergy/allergyfile19.txt', 'a+') as file:
-            file.write("Fish\n")
-            file.write(str('----------------\n'))
+            file.write("Fish, ")
     else:
         print("Nothing to do")
 
@@ -83,8 +88,7 @@ def recordOption():
     if CheckVar7.get()==1:
         print("Shellfish")
         with open('./allergy/allergyfile19.txt', 'a+') as file:
-            file.write("Shellfish\n")
-            file.write(str('----------------\n'))
+            file.write("Shellfish, ")
     else:
         print("Nothing to do")
 
@@ -92,8 +96,7 @@ def recordOption():
     if CheckVar8.get()==1:
         print("Molluscs")
         with open('./allergy/allergyfile19.txt', 'a+') as file:
-            file.write("Molluscs\n")
-            file.write(str('----------------\n'))
+            file.write("Molluscs, ")
     else:
         print("Nothing to do")
 
@@ -101,8 +104,7 @@ def recordOption():
     if CheckVar9.get()==1:
         print("Groundnut")
         with open('./allergy/allergyfile19.txt', 'a+') as file:
-            file.write("Groundnut\n")
-            file.write(str('----------------\n'))
+            file.write("Groundnut, ")
     else:
         print("Nothing to do")
 
@@ -110,8 +112,7 @@ def recordOption():
     if CheckVar10.get()==1:
         print("Oleaginous")
         with open('./allergy/allergyfile19.txt', 'a+') as file:
-            file.write("Oleaginous\n")
-            file.write(str('----------------\n'))
+            file.write("Oleaginous, ")
     else:
         print("Nothing to do")
 
@@ -119,8 +120,7 @@ def recordOption():
     if CheckVar11.get()==1:
         print("Sesame")
         with open('./allergy/allergyfile19.txt', 'a+') as file:
-            file.write("Sesame\n")
-            file.write(str('----------------\n'))
+            file.write("Sesame, ")
     else:
         print("Nothing to do")
 
@@ -128,8 +128,7 @@ def recordOption():
     if CheckVar12.get()==1:
         print("Soya")
         with open('./allergy/allergyfile19.txt', 'a+') as file:
-            file.write("Soya\n")
-            file.write(str('----------------\n'))
+            file.write("Soya, ")
     else:
         print("Nothing to do")
 
@@ -137,8 +136,7 @@ def recordOption():
     if CheckVar13.get()==1:
         print("Cereals")
         with open('./allergy/allergyfile19.txt', 'a+') as file:
-            file.write("Cereals\n")
-            file.write(str('----------------\n'))
+            file.write("Cereals, ")
     else:
         print("Nothing to do")
 
@@ -146,8 +144,7 @@ def recordOption():
     if CheckVar14.get()==1:
         print("Latex")
         with open('./allergy/allergyfile19.txt', 'a+') as file:
-            file.write("Latex\n")
-            file.write(str('----------------\n'))
+            file.write("Latex, ")
     else:
         print("Nothing to do")
 
@@ -155,8 +152,7 @@ def recordOption():
     if CheckVar15.get()==1:
         print("Rosacea")
         with open('./allergy/allergyfile19.txt', 'a+') as file:
-            file.write("Rosacea\n")
-            file.write(str('----------------\n'))
+            file.write("Rosacea, ")
     else:
         print("Nothing to do")
 
@@ -164,13 +160,17 @@ def recordOption():
     if CheckVar16.get()==1:
         print("Umbellifers")
         with open('./allergy/allergyfile19.txt', 'a+') as file:
-            file.write("Umbellifers\n")
-            file.write(str('----------------\n'))
+            file.write("Umbellifers, ")
     else:
         print("Nothing to do")
 
+    subprocess.run('./nutrition/updatepatient19.py', check=True)
+
 def confRec():
-    MsgBox2 = messagebox.showinfo("Confirmation", "Record confirmed and finished !")
+    """
+        To confirm that rec is finish
+    """
+    messagebox.showinfo("Confirmation", "Record confirmed and finished !")
 
 Intolabel = Label(gui, text="Intolerances : ", font="Times 18 bold",
     width=14, fg='aquamarine', bg='RoyalBlue4')
@@ -186,141 +186,140 @@ entryName = Entry(gui, textvariable=text_entry)
 entryName.grid(sticky='e', row=0, column=0, padx=10, pady=10)
 
 CheckVar1 = IntVar()
-C1 = Checkbutton(gui, text="Gluten", fg='navy', 
-    bg='dark turquoise', variable=CheckVar1, 
-    onvalue=1, offvalue=0, height=1, 
+C1 = Checkbutton(gui, text="Gluten", fg='navy',
+    bg='dark turquoise', variable=CheckVar1,
+    onvalue=1, offvalue=0, height=1,
     width=40, anchor="w")
 C1.grid(row=2, column=0)
 
 CheckVar2 = IntVar()
-C2 = Checkbutton(gui, text="Lactose", fg='navy', 
-    bg='dark turquoise', variable=CheckVar2, 
-    onvalue=1, offvalue=0, height=1, 
+C2 = Checkbutton(gui, text="Lactose", fg='navy',
+    bg='dark turquoise', variable=CheckVar2,
+    onvalue=1, offvalue=0, height=1,
     width=40, anchor="w")
 C2.grid(row=3, column=0)
 
 CheckVar3 = IntVar()
-C3 = Checkbutton(gui, text="Saccharose", fg='navy', 
-    bg='dark turquoise', variable=CheckVar3, 
-    onvalue=1, offvalue=0, height=1, 
+C3 = Checkbutton(gui, text="Saccharose", fg='navy',
+    bg='dark turquoise', variable=CheckVar3,
+    onvalue=1, offvalue=0, height=1,
     width=40, anchor="w")
 C3.grid(row=4, column=0)
 
 CheckVar4 = IntVar()
-C4 = Checkbutton(gui, text="Fructose", fg='navy', 
-    bg='dark turquoise', variable=CheckVar4, 
-    onvalue=1, offvalue=0, height=1, 
+C4 = Checkbutton(gui, text="Fructose", fg='navy',
+    bg='dark turquoise', variable=CheckVar4,
+    onvalue=1, offvalue=0, height=1,
     width=40, anchor="w")
 C4.grid(row=5, column=0)
 
-#Les allergies d’origine animale : 
+#Les allergies d’origine animale :
 animallabel = Label(gui, text="Animal allergy", font="Times 18 bold",
     fg='aquamarine', bg='RoyalBlue4')
-animallabel.grid(row=6, column=0, pady=10)  
+animallabel.grid(row=6, column=0, pady=10)
 
 CheckVar5 = IntVar()
-C5 = Checkbutton(gui, text="Eggs", fg='navy', 
-    bg='dark turquoise', variable=CheckVar5, 
-    onvalue=1, offvalue=0, height=1, 
+C5 = Checkbutton(gui, text="Eggs", fg='navy',
+    bg='dark turquoise', variable=CheckVar5,
+    onvalue=1, offvalue=0, height=1,
     width=40, anchor="w")
 C5.grid(row=7, column=0)
 
 CheckVar6 = IntVar()
-C6 = Checkbutton(gui, text="Fish", fg='navy', 
-    bg='dark turquoise', variable=CheckVar6, 
-    onvalue=1, offvalue=0, height=1, 
+C6 = Checkbutton(gui, text="Fish", fg='navy',
+    bg='dark turquoise', variable=CheckVar6,
+    onvalue=1, offvalue=0, height=1,
     width=40, anchor="w")
 C6.grid(row=8, column=0)
 
-
 CheckVar7 = IntVar()
-C7 = Checkbutton(gui, text="Shellfish", fg='navy', 
-    bg='dark turquoise', variable=CheckVar7, 
-    onvalue=1, offvalue=0, height=1, 
+C7 = Checkbutton(gui, text="Shellfish", fg='navy',
+    bg='dark turquoise', variable=CheckVar7,
+    onvalue=1, offvalue=0, height=1,
     width=40, anchor="w")
 C7.grid(row=9, column=0)
 
 CheckVar8 = IntVar()
-C8 = Checkbutton(gui, text="Molluscs", fg='navy', 
-    bg='dark turquoise', variable=CheckVar8, 
-    onvalue=1, offvalue=0, height=1, 
+C8 = Checkbutton(gui, text="Molluscs", fg='navy',
+    bg='dark turquoise', variable=CheckVar8,
+    onvalue=1, offvalue=0, height=1,
     width=40, anchor="w")
 C8.grid(row=10, column=0)
 
-#Les allergies d’origine végétale : 
+#Les allergies d’origine végétale :
 vegetallabel = Label(gui, text="Vegetable allergy",
     font="Times 18 bold", fg='aquamarine', bg='RoyalBlue4')
-vegetallabel.grid(row=11, column=0, pady=10)  
+vegetallabel.grid(row=11, column=0, pady=10)
 
 CheckVar9 = IntVar()
-C9 = Checkbutton(gui, text="Groundnut", fg='navy', 
-    bg='dark turquoise', variable=CheckVar9, 
-    onvalue=1, offvalue=0, height=1, 
+C9 = Checkbutton(gui, text="Groundnut", fg='navy',
+    bg='dark turquoise', variable=CheckVar9,
+    onvalue=1, offvalue=0, height=1,
     width=40, anchor="w")
 C9.grid(row=12, column=0)
 
 CheckVar10 = IntVar()
-C10 = Checkbutton(gui, text="Oleaginous fruits", fg='navy', 
-    bg='dark turquoise', variable=CheckVar10, 
-    onvalue=1, offvalue=0, height=1, 
+C10 = Checkbutton(gui, text="Oleaginous fruits", fg='navy',
+    bg='dark turquoise', variable=CheckVar10,
+    onvalue=1, offvalue=0, height=1,
     width=40, anchor="w")
 C10.grid(row=13, column=0)
 
 CheckVar11 = IntVar()
-C11 = Checkbutton(gui, text="Sesame", fg='navy', 
-    bg='dark turquoise', variable=CheckVar11, 
-    onvalue=1, offvalue=0, height=1, 
+C11 = Checkbutton(gui, text="Sesame", fg='navy',
+    bg='dark turquoise', variable=CheckVar11,
+    onvalue=1, offvalue=0, height=1,
     width=40, anchor="w")
 C11.grid(row=14, column=0)
 
 CheckVar12 = IntVar()
-C12 = Checkbutton(gui, text="Soya", fg='navy', 
-    bg='dark turquoise', variable=CheckVar12, 
-    onvalue=1, offvalue=0, height=1, 
+C12 = Checkbutton(gui, text="Soya", fg='navy',
+    bg='dark turquoise', variable=CheckVar12,
+    onvalue=1, offvalue=0, height=1,
     width=40, anchor="w")
 C12.grid(row=15, column=0)
 
 CheckVar13 = IntVar()
-C13 = Checkbutton(gui, text="Cereals (wheat, rye)", fg='navy', 
-    bg='dark turquoise', variable=CheckVar13, 
-    onvalue=1, offvalue=0, height=1, 
+C13 = Checkbutton(gui, text="Cereals (wheat, rye)", fg='navy',
+    bg='dark turquoise', variable=CheckVar13,
+    onvalue=1, offvalue=0, height=1,
     width=40, anchor="w")
 C13.grid(row=16, column=0)
 
 CheckVar14 = IntVar()
-C14 = Checkbutton(gui, text="Latex fruits", fg='navy', 
-    bg='dark turquoise', variable=CheckVar14, 
-    onvalue=1, offvalue=0, height=1, 
+C14 = Checkbutton(gui, text="Latex fruits", fg='navy',
+    bg='dark turquoise', variable=CheckVar14,
+    onvalue=1, offvalue=0, height=1,
     width=40, anchor="w")
 C14.grid(row=17, column=0)
 
 latexlabel = Label(gui, text="Latex fruits = avocado, banana, kiwi,"
     " fig, chestnut...", font="Times 12", fg='aquamarine',
     bg='RoyalBlue4')
-latexlabel.grid(row=18, column=0, pady=10)  
+latexlabel.grid(row=18, column=0, pady=10)
 
 CheckVar15 = IntVar()
 C15 = Checkbutton(gui, text="Rosacea",
-    fg='navy', bg='dark turquoise', variable=CheckVar15, 
-    onvalue=1, offvalue=0, height=1, 
+    fg='navy', bg='dark turquoise', variable=CheckVar15,
+    onvalue=1, offvalue=0, height=1,
     width=40, anchor="w")
 C15.grid(row=19, column=0)
 
 rosaclabel = Label(gui, text="Rosacea = apricot, cherry, strawberry...",
     font="Times 12", fg='aquamarine', bg='RoyalBlue4')
-rosaclabel.grid(row=20, column=0, pady=10)  
+rosaclabel.grid(row=20, column=0, pady=10)
 
 CheckVar16 = IntVar()
-C16 = Checkbutton(gui, text="Umbellifers", fg='navy', 
-    bg='dark turquoise', variable=CheckVar16, 
-    onvalue=1, offvalue=0, height=1, 
+C16 = Checkbutton(gui, text="Umbellifers", fg='navy',
+    bg='dark turquoise', variable=CheckVar16,
+    onvalue=1, offvalue=0, height=1,
     width=40, anchor="w")
 C16.grid(row=21, column=0)
 
 ombellabel = Label(gui, text="Umbellifers = dill, carrot, celery,"
     " fennel, parsley...",
     font="Times 12", fg='aquamarine', bg='RoyalBlue4')
-ombellabel.grid(row=22, column=0, pady=10)  
+ombellabel.grid(row=22, column=0, pady=10)
 
 buttSave = Button(gui, text="Save", width=10, fg='yellow',
     bg='RoyalBlue3', bd=3, highlightbackground='RoyalBlue4',

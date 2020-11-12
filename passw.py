4d85a3b303406d675948e63a4a6515dc8429f3f5
TLRKiliann <philogenie@protonmail.com>
2020-11-12 10:56:26 +0100

    password app added

---
+++ passw.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import messagebox
+from subprocess import run
+from heal_track import Application
+
+
+window = Tk()
+window.title('ACCESS')
+window.configure(bg='DodgerBlue2')
+
+def closeWindow():
+	"""
+	    Class call !
+	"""
+	window.destroy()
+	Application()
+
+def validentry():
+	"""
+	    To validate entree
+	    from user.
+	"""
+	namenter = entryname.get()
+	passentry = entrypass.get()
+	MSGBox = messagebox.askyesno('INFO', 'Do you want to validate for access ?')
+	if MSGBox == 1:
+		if entryname.get() == "ck" and entrypass.get() == "123":
+			messagebox.showinfo('INFO', 'Right, you get access !')
+			closeWindow()
+		else:
+			messagebox.showinfo('INFO', 'Password or Username failed !')
+			window.destroy()
+
+labelname = Label(window, text='Enter username :')
+labelname.pack(pady=10)
+
+entryname = StringVar()
+namenter = Entry(window, textvariable=entryname)
+namenter.pack(padx=10)
+
+labelpass = Label(window, text='Enter password :')
+labelpass.pack(pady=10)
+
+entrypass = StringVar()
+passentry = Entry(window, textvariable=entrypass)
+passentry.pack(padx=10)
+
+buttonvalidate = Button(window, text='Enter', command=validentry)
+buttonvalidate.pack(pady=10)
+
+mainloop()

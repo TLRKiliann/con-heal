c81379ba31d1be87a8ba0eb92a7eb207e6e216fc
TLRKiliann <philogenie@protonmail.com>
2020-12-04 19:51:57 +0100

    contact1 is ok

--- contact/pat_contact1.py
+++ contact/pat_contact1.py
@@ -3,6 +3,7 @@
 
 
 from tkinter import *
+import os
 
 
 def Window(self):
@@ -22,47 +23,61 @@ def Window(self):
                 line1 = namefile.readline()
                 line2 = namefile.readline()
         except FileNotFoundError as fileout:
-            print("No file entryfile.txt exist", fileout)
+            print("+ No file entryfile.txt exist", fileout)
 
         try:
-            with open('./contact/contact1.txt', 'r') as policyfile:
-                native = policyfile.readline()
-                phone = policyfile.readline()
-                street = policyfile.readline()
-                state = policyfile.readline()
-                email = policyfile.readline()
-                assu = policyfile.readline()
-                polins = policyfile.readline()
+            if os.path.getsize('./contact/contact1.txt'):
+                print("+ Ok, contact1.txt exist")
+        except FileNotFoundError as errfnf:
+            print("+ No file contact1.txt exist", errfnf)
+            with open('./contact/contact1.txt', 'a+') as testf:
+                print("+ File contact1.txt created !")
+
+        try:
+            if os.path.exists('./contact/contact1.txt'):
+                with open('./contact/contact1.txt', 'r') as policyfile:
+                    line1 = policyfile.readline()
+                    line2 = policyfile.readline()
+                    native = policyfile.readline()
+                    phone = policyfile.readline()
+                    street = policyfile.readline()
+                    state = policyfile.readline()
+                    email = policyfile.readline()
+                    assu = policyfile.readline()
+                    polins = policyfile.readline()
+            else:
+                pass
         except FileNotFoundError as err_r:
-            print("No file contact1.txt exist", err_r)
+            print("+ No file contact1.txt exist", err_r)
 
         self.x1, self.y1 = 950, 350
         self.txtBox = Text(self.can, height=25, width=40, font=18, relief=SUNKEN)
         self.txtBox.delete('1.0', END)
         self.txtBox.update()
-        self.txtBox.insert(INSERT, "Patient Name : " + line1 + "\n\n")
-        self.txtBox.insert(INSERT, "Birthdate : " + line2 + "\n\n")
-        self.txtBox.insert(INSERT, "Native : " + native + "\n\n")
-        self.txtBox.insert(INSERT, "Phone : " + phone + "\n\n")
-        self.txtBox.insert(INSERT, "Street : " + street + "\n\n")
-        self.txtBox.insert(INSERT, "City : " + state + "\n\n")
-        self.txtBox.insert(INSERT, "e-mail : " + email + "\n\n")
-        self.txtBox.insert(INSERT, "Insurance : " + assu + "\n\n")
-        self.txtBox.insert(INSERT, "Policy : " + polins + "\n\n")
+        self.txtBox.insert(INSERT, line1 + "\n\n")
+        self.txtBox.insert('2.0', line2 + "\n\n")
+        self.txtBox.insert('4.0', native + "\n\n")
+        self.txtBox.insert('6.0', phone + "\n\n")
+        self.txtBox.insert('8.0', street + "\n\n")
+        self.txtBox.insert('9.0', state + "\n\n")
+        self.txtBox.insert('11.0', email + "\n\n")
+        self.txtBox.insert('13.0', assu + "\n\n")
+        self.txtBox.insert('15.0', polins + "\n\n")
         self.ftxtBox_window = self.can.create_window(self.x1, self.y1, window=self.txtBox)
 
-    def recorderData(native, nativaentry, txtphone, phonentry,
-        addrtxt, entryaddr, citytxt, entrycity, mailtxt, entrymail,
+    def recorderData(namentry, birthentry, native, nativaentry, txtphone, phonentry,
+        addrtxt, addrentry, citytxt, cityentry, mailtxt, entrymail,
         assurance, entryassu, policy, entrypolicy):
         """
             Display origin
         """
         try:
-            with open('./newpatient/entryfile.txt', 'r') as namefile:
-                line1 = namefile.readline()
-                line2 = namefile.readline()
-        except FileNotFoundError as fileout:
-            print("No file entryfile.txt exist", fileout)
+            if os.path.getsize('./contact/contact1.txt'):
+                print("+ Ok, contact1.txt exist")
+        except FileNotFoundError as errfnf:
+            print("+ No file contact1.txt exist", errfnf)
+            with open('./contact/contact1.txt', 'a+') as testf:
+                print("+ File contact1.txt created !")
 
         self.x1, self.y1 = 950, 350
         self.txtBox = Text(self.can, height=25, width=40, font=18, relief=SUNKEN)
@@ -74,29 +89,29 @@ def Window(self):
 
         try:
             with open('./contact/contact1.txt', 'w') as iofile:
-                iofile.write("Name : " + line1 + '\n')
-                iofile.write("Birthdate : " + line2 + "\n")
-                self.txtBox.insert('1.0', "Patient Name : " + line1 + "\n\n")
+                iofile.write("Name : " + namentry.get())
+                iofile.write("Birthdate : " + line2)
+                self.txtBox.insert(INSERT, "Patient Name : " + namentry.get() + "\n\n")
                 self.txtBox.insert('2.0', "Birthdate : " + line2 + "\n\n")
-                iofile.write("Native : " + nativentry.get() + '\n')
-                self.txtBox.insert('4.0', "Native : " + nativentry.get() + "\n\n")
-                iofile.write("Phone : " + phonentry.get() + '\n')
+                iofile.write("Native : " + nativaentry.get() + "\n")
+                self.txtBox.insert('4.0', "Native : " + nativaentry.get() + "\n\n")
+                iofile.write("Phone : " + phonentry.get() + "\n")
                 self.txtBox.insert('6.0', "Phone : " + phonentry.get() + "\n\n")
-                iofile.write("Address : " + entryaddr.get() + '\n')
-                self.txtBox.insert('8.0', "Address : " + entryaddr.get() + "\n\n")
-                iofile.write("City : " + entrycity.get() + '\n')
-                self.txtBox.insert('9.0', "City : " + entrycity.get() + "\n\n")
-                iofile.write("e-mail : " + entrymail.get() + '\n')
-                self.txtBox.insert('11', "e-mail : " + entrymail.get() + "\n\n")
-                iofile.write("Insurance : " + entryassu.get() + '\n')
-                self.txtBox.insert('13', "Insurance : " + entryassu.get() + "\n\n")
-                iofile.write("Policy : " + entrypolicy.get() + '\n')
-                self.txtBox.insert('15', "Policy : " + entrypolicy.get() + "\n\n")
+                iofile.write("Address : " + addrentry.get() + "\n")
+                self.txtBox.insert('8.0', "Address : " + addrentry.get() + "\n\n")
+                iofile.write("City : " + cityentry.get() + "\n")
+                self.txtBox.insert('9.0', "City : " + cityentry.get() + "\n\n")
+                iofile.write("e-mail : " + entrymail.get() + "\n")
+                self.txtBox.insert('11.0', "e-mail : " + entrymail.get() + "\n\n")
+                iofile.write("Insurance : " + entryassu.get() + "\n")
+                self.txtBox.insert('13.0', "Insurance : " + entryassu.get() + "\n\n")
+                iofile.write("Policy : " + entrypolicy.get() + "\n")
+                self.txtBox.insert('15.0', "Policy : " + entrypolicy.get() + "\n\n")
         except FileNotFoundError as err_w:
-            print("No file contact1.txt exist", err_w)
+            print("+ No file contact1.txt exist", err_w)
 
     allInData()
-    
+
     # Label title
     self.x11, self.y11 = 200, 100
     self.lbltitle = Label(self.can, text="Contact",
@@ -118,7 +133,7 @@ def Window(self):
             line1 = namefile.readline()
             txt_pat = line1
     except FileNotFoundError as callfile:
-        print("File entryfile.txt doesn't exist !", callfile)
+        print("+ File entryfile.txt doesn't exist !", callfile)
 
     try:
         self.txt_pat = line1
@@ -130,7 +145,27 @@ def Window(self):
         self.namentry_window = self.can.create_window(self.x2, self.y2,
             window = self.namentry)
     except UnboundLocalError as ub_error1:
-        print("+ File 1 not created !", ub_error1)
+        print("+ + File 1 not created !", ub_error1)
+
+    try:
+        with open('./newpatient/entryfile.txt', 'r') as namefile:
+            line1 = namefile.readline()
+            line2 = namefile.readline()
+            birth_pat = line2
+    except FileNotFoundError as callfile:
+        print("+ File entryfile.txt doesn't exist !", callfile)
+
+    try:
+        self.txt_pat = line1
+        self.x2, self.y2 = 400, 200
+        self.birth_pat = StringVar()
+        self.birthentry = Entry(self.can, textvariable=self.birth_pat,
+            highlightbackground='grey', bd=4)
+        self.birth_pat.set(line1)
+        self.birthentry_window = self.can.create_window(self.x2, self.y2,
+            window = self.birthentry)
+    except UnboundLocalError as ub_error1:
+        print("+ + File 1 not created !", ub_error1)
 
     # Native
     self.x15, self.y15 = 200, 250
@@ -237,9 +272,11 @@ def Window(self):
         width=8, bd=3, bg='RoyalBlue3', fg='yellow',
         highlightbackground='RoyalBlue3',
         activebackground='pale turquoise',
-        command = lambda: recorderData(self.native, self.nativaentry, self.txtphone, self.phonentry,
-        self.addrtxt, self.entryaddr, self.citytxt, self.entrycity, self.mailtxt, self.entrymail,
-        self.assurance, self.entryassu, self.policy, self.entrypolicy))
+        command = lambda: recorderData(self.namentry, self.birthentry, 
+            self.native, self.nativaentry, self.txtphone, self.phonentry,
+            self.addrtxt, self.addrentry, self.citytxt, self.cityentry,
+            self.mailtxt, self.entrymail, self.assurance, self.entryassu,
+            self.policy, self.entrypolicy))
     self.fb52_window = self.can.create_window(self.x52, self.y52, window=self.b52)
 
     self.can.configure(scrollregion=self.can.bbox(ALL))

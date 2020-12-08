c2880ed3f40f92b831108a76d130ce8a7a103dfa
TLRKiliann <philogenie@protonmail.com>
2020-12-08 22:26:01 +0100

    14 needs suivi_patient 1+2 ok

--- 14besoins/suivi_patient_1.py
+++ 14besoins/suivi_patient_1.py
@@ -28,8 +28,8 @@ class MenuBar(Frame):
     """Barre menu déroulant"""
     def __init__(self, boss=None):
         Frame.__init__(self, borderwidth=5, bg='RoyalBlue3', padx=0)
-        But2=Button(self, text ="Close", fg='white', bg='blue', relief=GROOVE,
-            activebackground='cyan', command=boss.quit).pack(side=LEFT, padx=3)
+        But2=Button(self, text ="Close", fg='white', bg='RoyalBlue3', relief=GROOVE,
+            activebackground='pale turquoise', command=boss.quit).pack(side=LEFT, padx=3)
 
 # Application principale
 class Application(Frame):
@@ -39,7 +39,7 @@ class Application(Frame):
         mBar=MenuBar(self)
         mBar.pack(side=TOP, fill=X, expand=1)
         # ScrollCanvas limite de la zone à parcourir avec la barre
-        self.can=Canvas(self, width=600, height=400, bg='cyan')
+        self.can=Canvas(self, width=600, height=400, bg='DodgerBlue2')
         self.frame = Frame(self.can)
         self.vsb = Scrollbar(self, orient=VERTICAL, command=self.can.yview)
         self.can.configure(yscrollcommand=self.vsb.set)
@@ -50,7 +50,7 @@ class Application(Frame):
         # Insertion du texte
         self.can.create_text(300, 150, anchor=CENTER, text="Care and Monitoring",
             font=('Times New Roman', 28), fill='navy')
-        self.can.create_text(590, 380, anchor=NE, text="Copyright (C) 2020 Inc.",
+        self.can.create_text(590, 380, anchor=NE, text="14 needs",
             font=('Times', 12), fill='white') 
         self.can.pack(side=LEFT, fill=BOTH, expand=1)
         # Configuration de la Scrollbar sur le Frame
@@ -58,13 +58,13 @@ class Application(Frame):
 
         self.x2, self.y2 = 200, 250
         self.b2=Button(self.can, width=10, font=16, bg='RoyalBlue3', fg='gold',
-            activebackground='dark turquoise', bd=3, highlightbackground='light sky blue', 
+            activebackground='pale turquoise', bd=3, highlightbackground='light sky blue', 
             text="Add", command=self.lienDirect)
         self.fb2=self.can.create_window(self.x2, self.y2, window=self.b2)
 
         self.x3, self.y3 = 400, 250
         self.b3=Button(self.can, width=10, font=16, bg='RoyalBlue3', fg='gold',
-            activebackground='dark turquoise', bd=3, highlightbackground='light sky blue', 
+            activebackground='pale turquoise', bd=3, highlightbackground='light sky blue', 
             text="Read", command=self.lectureFic)
         self.fb3=self.can.create_window(self.x3, self.y3, window=self.b3)
         self.pack()

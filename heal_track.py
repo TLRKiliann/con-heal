f18a7a58856d995e0371619e2d57f3ff6c951376
TLRKiliann <philogenie@protonmail.com>
2020-12-10 17:43:25 +0100

    transparency window on work !

--- heal_track.py
+++ heal_track.py
@@ -1554,24 +1554,12 @@ class Application(Frame):
     def __init__(self, boss=None):
         Frame.__init__(self, borderwidth=5, bg='RoyalBlue4', padx=20, pady=20, relief=GROOVE)
         self.master.title('Time-Track Developed by ko@l@tr33 - 2020')
-        self.master.resizable(False, False)
-        print(self.master.winfo_screenheight())
-        print(self.master.winfo_screenwidth())
-        #size_h=1050
-        #size_w=1680
-        self.window_height = 824
-        self.window_width = 1310
-
-        self.screen_width = self.master.winfo_screenwidth()
-        self.screen_height = self.master.winfo_screenheight()
-
-        self.x_cordinate = int((self.screen_width/2) - (self.window_width/2))
-        self.y_cordinate = int((self.screen_height/2) - (self.window_height/2))
-
-        self.master.geometry("{}x{}+{}+{}".format(self.window_width, self.window_height,
-            self.x_cordinate, self.y_cordinate))
-        print(self.x_cordinate)
-        print(self.y_cordinate)
+        self.master.withdraw()
+        self.master.update_idletasks()  # Update "requested size" from geometry manager
+        x = (self.master.winfo_screenwidth() / 3 - self.master.winfo_reqwidth()) / 2
+        y = (self.master.winfo_screenheight() / 3 - self.master.winfo_reqheight()) / 2
+        self.master.geometry("+%d+%d" % (x, y))
+        self.master.deiconify()
 
         self.mBar = MenuBar(self)
         self.mBar.pack(side=TOP, fill=X, expand=YES)
@@ -2013,11 +2001,16 @@ class Application(Frame):
     def contact_num24(self):
         subprocess.run('./contact/pat_contact24.py', check=True)
 
-    # CheckBox 14 needs OK
-    def besoinsCoche(self):
-        self.master.withdraw()
+    def trancparency(self):
         subprocess.run('./14besoins/checkb.py', check=True)
-        self.master.deiconify()
+        #self.master.wm_attributes('-alpha', 0.8)
+
+    # CheckBox 14 needs OK
+    # ON WORK !!!
+    def besoinsCoche(self):
+        self.master.wm_attributes('-alpha', 0.8)
+        self.trancparency()
+        #subprocess.run('./14besoins/checkb.py', check=True)
 
     def besoins2Coche(self):
         self.master.withdraw()

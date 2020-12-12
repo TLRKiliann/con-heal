88505f710b17dcb04c0b6cd32b7b50792cbcd53b
TLRKiliann <philogenie@protonmail.com>
2020-12-12 23:28:11 +0100

    diag_patient 21-24 ok

--- diag/diag_patient21.py
+++ diag/diag_patient21.py
@@ -12,7 +12,7 @@ import subprocess
 class ScrollCanvas(Frame):
     def __init__(self, boss=None):
         Frame.__init__(self, borderwidth=borderwidth, relief=relief)
-        self.can=Canvas(self, width=width, height=height, bd=bd, bg=bg,
+        self.can = Canvas(self, width=width, height=height, bd=bd, bg=bg,
             relief=relief)
         self.frame = Frame(self.can)
 
@@ -31,19 +31,19 @@ class MenuBar(Frame):
     def __init__(self, boss=None):
         Frame.__init__(self, borderwidth=5, bg='RoyalBlue3', padx=0)
         # Menu fichier
-        But=Button(self, text ="Close", fg='white', bg='blue',
-            activebackground='cyan', command=boss.quit).pack(side=LEFT,
+        But = Button(self, text ="Close", fg='cyan', bg='RoyalBlue3',
+            activebackground='pale turquoise', command=boss.quit).pack(side=LEFT,
             padx=3)
 
 # Application principale
 class Application(Frame):
     def __init__(self, boss=None):
         Frame.__init__(self)
-        self.master.title('TIME-TRACK - Developed by ko@l@tr33 - 2020')
-        mBar=MenuBar(self)
+        self.master.title('Diagnosis')
+        mBar = MenuBar(self)
         mBar.pack(side=TOP, fill=X, expand=1)
         # ScrollCanvas limite de la zone à parcourir avec la barre
-        self.can=Canvas(self, width=600, height=400, bg='cyan')
+        self.can = Canvas(self, width=600, height=400, bg='DodgerBlue2')
         self.frame = Frame(self.can)
         self.vsb = Scrollbar(self, orient=VERTICAL, command=self.can.yview)
         self.can.configure(yscrollcommand=self.vsb.set)
@@ -54,25 +54,25 @@ class Application(Frame):
         # Insertion du texte
         self.can.create_text(300, 140, anchor=CENTER, text="Diagnostics and ATCD",
                     font=('Times New Roman', 28), fill='navy')
-        self.can.create_text(590, 380, anchor=NE, text="TIME-TRACK",
+        self.can.create_text(590, 380, anchor=NE, text="Diagnosis + Update",
                     font=('Times', 12), fill='white') 
         self.can.pack(side=LEFT, fill=BOTH, expand=1)
         # Configuration de la Scrollbar sur le Frame
         self.frame.bind("<Configure>", self.onFrameConfigure)
         # Création des boutons
         self.x2, self.y2 = 200, 250
-        self.b2=Button(self.can, width=10, font=16, bg='RoyalBlue3', fg='gold',
-            activebackground='dark turquoise', bd=3, 
+        self.b2 = Button(self.can, width=10, font=16, bg='RoyalBlue3', fg='gold',
+            activebackground='pale turquoise', bd=3, 
             highlightbackground='light sky blue', 
             text="Add", command=self.Frame_Ap1)
-        self.fb2=self.can.create_window(self.x2, self.y2, window=self.b2)
+        self.fb2 = self.can.create_window(self.x2, self.y2, window=self.b2)
 
         self.x3, self.y3 = 400, 250
-        self.b3=Button(self.can, width=10, font=16, bg='RoyalBlue3', fg='gold',
-            activebackground='dark turquoise', bd=3, 
+        self.b3 = Button(self.can, width=10, font=16, bg='RoyalBlue3', fg='gold',
+            activebackground='pale turquoise', bd=3, 
             highlightbackground='light sky blue', 
             text="Read", command=self.Frame_Ap2)
-        self.fb3=self.can.create_window(self.x3, self.y3, window=self.b3)
+        self.fb3 = self.can.create_window(self.x3, self.y3, window=self.b3)
         self.pack()
 
     # Méthode pour reconfigurer la scrollbar à chaque fois
@@ -82,7 +82,7 @@ class Application(Frame):
 
     def Frame_Ap1(self):
         """
-        To verify and write in diag file
+            To verify and write in diag file
         """
         try:
             if os.path.getsize('./diag/doc_diag21/diagrecap21.txt'):
@@ -97,7 +97,7 @@ class Application(Frame):
 
     def Frame_Ap2(self):
         """
-        To verify and read diag file
+            To verify and read diag file
         """
         try:
             if os.path.getsize('./diag/doc_diag21/diagrecap21.txt'):

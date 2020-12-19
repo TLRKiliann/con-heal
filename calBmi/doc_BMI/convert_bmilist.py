d24295a3f7a43634d915f5ff52f62f1788d37c8a
TLRKiliann <philogenie@protonmail.com>
2020-12-19 01:17:56 +0100

    bmi+kg improved to show values

--- calBmi/doc_BMI/convert_bmilist.py
+++ calBmi/doc_BMI/convert_bmilist.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# -*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
 
 
 import os
@@ -19,21 +19,24 @@ file = open('./calBmi/doc_BMI/file_bmi.json')
 data = json.load(file)
 #file.close
 
-for (key, value) in data.items():
-    print("Key: " + key)
-    print("Valeur: " + str(value))
-    print("\nTo represent the data_get:\n")
-    print(data.get("data"))
-    print("\n")
-    print("Valeur: " + str(value[0]))
-    print("Valeur: " + str(value[1]))
-    print("\n")
-    print("Date: " + str(value[0]["Date"]))
-    print("BMI: " + str(value[0]["BMI"]))
-    print("\n")
-    print("Date: " + str(value[1]["Date"]))
-    print("BMI: " + str(value[1]["BMI"]))
-    
+try:
+    for (key, value) in data.items():
+        print("Key: " + key)
+        print("Valeur: " + str(value))
+        print("\nTo represent the data_get:\n")
+        print(data.get("data"))
+        print("\n")
+        print("Valeur: " + str(value[0]))
+        print("Valeur: " + str(value[1]))
+        print("\n")
+        print("Date: " + str(value[0]["Date"]))
+        print("BMI: " + str(value[0]["BMI"]))
+        print("\n")
+        print("Date: " + str(value[1]["Date"]))
+        print("BMI: " + str(value[1]["BMI"]))
+except IndexError as ind_err:
+    print("Only one value for BMI...", ind_err)
+
 data_list1 = []
 for value in zip(value):
     data_list1.append(value[0]['Date'])
@@ -94,7 +97,6 @@ with plt.style.context(('dark_background')):
     plt.ylabel('BMI', fontsize=14)
     plt.xlabel('Dates', fontsize=14)
     plt.title('BMI by Date', fontsize=16)
-    #plt.xticks(rotation=45)
     plt.legend(['BMI/date'])
     plt.grid(show_grid)
     plt.gcf().autofmt_xdate(rotation=45)

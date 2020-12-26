5d980640862a58bbd22d2538a5d4b57ab4cf6460
TLRKiliann <philogenie@protonmail.com>
2020-12-26 10:09:27 +0100

    stix added

--- labo/combtest23.py
+++ labo/combtest23.py
@@ -27,7 +27,7 @@ def saveMyButt():
 def recordOption():
     print("+ Date : " + time.strftime("%d/%m/%Y"))
     print("+ Nom du patient : ", textName.get())
-    with open('./14besoins/doc_suivi23/main_14b.txt', 'a+') as file:
+    with open('./14besoins/doc_suivi23/patient23_14b.txt', 'a+') as file:
         with open('./labo/doc_labo/result23.txt', 'a+') as file_2:
             file.write("\n\n***************************************************************************\n")
             file.write("Date : ")
--- labo/combtest24.py
+++ labo/combtest24.py
@@ -27,7 +27,7 @@ def saveMyButt():
 def recordOption():
     print("+ Date : " + time.strftime("%d/%m/%Y"))
     print("+ Nom du patient : ", textName.get())
-    with open('./14besoins/doc_suivi24/main_14b.txt', 'a+') as file:
+    with open('./14besoins/doc_suivi24/patient24_14b.txt', 'a+') as file:
         with open('./labo/doc_labo/result24.txt', 'a+') as file_2:
             file.write("\n\n***************************************************************************\n")
             file.write("Date : ")

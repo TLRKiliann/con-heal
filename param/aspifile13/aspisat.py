25a39c3ba3d9a7eedee658ed499a3229d5519e98
TLRKiliann <philogenie@protonmail.com>
2021-01-05 00:44:58 +0100

    param on work

---
+++ param/aspifile13/aspisat.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+import subprocess
+import json
+
+
+file = open('./param/aspifile13/sat.json')
+data = json.load(file)
+
+try:
+    for (key, value) in data.items():
+        print("Key: " + key)
+        print("Valeur: " + str(value))
+        print("\nTo represent the data_get:\n")
+        print(data.get("data"))
+        print("\n")
+        print("Valeur: " + str(value[0]))
+        print("\n")
+        print("Date: " + str(value[0]["Date"]))
+        print("SaO2: " + str(value[0]["SaO2"]))
+        print("\n")
+except IndexError as err_read:
+    print("+ Error from aspidata.py", err_read)
+
+print("\nList of dates\n")
+
+data_list1 = []
+for value in zip(value):
+    data_list1.append(value[0]['Date'])
+    print(value[0]['Date'])
+
+print("\nThat seems ok!\n")
+
+with open('./param/aspifile13/data_datesat.json', 'a+') as datafile:
+    json.dump(data_list1, datafile, indent=4)
+
+for (key, value) in data.items():
+    print(key, value)
+    print("\n")
+
+print("\nList of SaO2\n")
+
+data_list2 = []
+for value in zip(value):
+    data_list2.append(value[0]['SaO2'])
+    print(value[0]['SaO2'])
+
+print("\nThat seems correct!\n")
+
+with open('./param/aspifile13/data_sat.json', 'a+') as datafile:
+    json.dump(data_list2, datafile, indent=4)
+
+print("\nDownloading 'plot_prog.py'...")
+
+subprocess.run('./param/aspifile13/plot/plot_sat.py', check=True)
---
+++ param/aspifile21/aspigly.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+import subprocess
+import json
+
+
+file = open('./param/aspifile12/gly.json')
+data = json.load(file)
+
+try:
+    for (key, value) in data.items():
+        print("Key: " + key)
+        print("Valeur: " + str(value))
+        print("\nTo represent the data_get:\n")
+        print(data.get("data"))
+        print("\n")
+        print("Valeur: " + str(value[0]))
+        print("\n")
+        print("Date: " + str(value[0]["Date"]))
+        print("Glycemie: " + str(value[0]["Glycemie"]))
+        print("\n")
+except IndexError as err_read:
+    print("+ Error from aspidata.py", err_read)
+
+print("\nList of dates\n")
+
+data_list1 = []
+for value in zip(value):
+    data_list1.append(value[0]['Date'])
+    print(value[0]['Date'])
+
+print("\nThat seems ok!\n")
+
+with open('./param/aspifile12/data_dategly.json', 'a+') as datafile:
+    json.dump(data_list1, datafile, indent=4)
+
+for (key, value) in data.items():
+    print(key, value)
+    print("\n")
+
+print("\nList of glycemia\n")
+
+data_list2 = []
+for value in zip(value):
+    data_list2.append(value[0]['Glycemie'])
+    print(value[0]['Glycemie'])
+
+print("\nThat seems correct!\n")
+
+with open('./param/aspifile12/data_gly.json', 'a+') as datafile:
+    json.dump(data_list2, datafile, indent=4)
+
+print("\nDownloading 'plot_prog.py'...")
+
+subprocess.run('./param/aspifile12/plot/plot_gly.py', check=True)

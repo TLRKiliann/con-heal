e8d6cd6845c106f70b13dd609c3494a3a0e51612
TLRKiliann <philogenie@protonmail.com>
2021-01-05 18:42:27 +0100

    plots of param on work

--- param/aspifile24/aspidlr.py
+++ param/aspifile24/aspidlr.py
@@ -6,7 +6,7 @@ import subprocess
 import json
 
 
-file = open('./param/aspifile12/dlr.json')
+file = open('./param/aspifile24/dlr.json')
 data = json.load(file)
 
 try:
@@ -33,7 +33,7 @@ for value in zip(value):
 
 print("\nThat seems ok!\n")
 
-with open('./param/aspifile12/data_datedlr.json', 'a+') as datafile:
+with open('./param/aspifile24/data_datedlr.json', 'a+') as datafile:
     json.dump(data_list1, datafile, indent=4)
 
 for (key, value) in data.items():
@@ -49,9 +49,9 @@ for value in zip(value):
 
 print("\nThat seems correct!\n")
 
-with open('./param/aspifile12/data_dlr.json', 'a+') as datafile:
+with open('./param/aspifile24/data_dlr.json', 'a+') as datafile:
     json.dump(data_list2, datafile, indent=4)
 
 print("\nDownloading 'plot_prog.py'...")
 
-subprocess.run('./param/aspifile12/plot/plot_dlr.py', check=True)
+subprocess.run('./param/aspifile24/plot/plot_dlr.py', check=True)

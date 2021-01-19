454b4867e214e0b7c6e4151a7b8b190ca94d3979
TLRKiliann <philogenie@protonmail.com>
2021-01-19 23:18:50 +0100

    aspifile 11 ok

--- param/aspifile11/aspifreq.py
+++ param/aspifile11/aspifreq.py
@@ -52,6 +52,17 @@ print("\nThat seems correct!\n")
 with open('./param/aspifile11/data_fr.json', 'a+') as datafile:
     json.dump(data_list2, datafile, indent=4)
 
-print("\nDownloading 'plot_prog.py'...")
+try:
+    proc = subprocess.run(["scp", "./param/paramdata11.txt",
+        "pi@192.168.18.12:~/tt_doc/doc_txt11/paramdata11.txt"],
+        stderr=subprocess.PIPE)
+    print("Result SCP transfert : %s" % repr(proc.stderr))
+    secproc = subprocess.run(["scp", "./param/aspifile11/freq.json",
+        "pi@192.168.18.12:~/tt_doc/doc_txt11/freq.json"],
+        stderr=subprocess.PIPE)
+    print("Result SCP transfert : %s" % repr(secproc.stderr))
+except (OSError, FileNotFoundError) as e_failed:
+    print("+ SCP transfert (upload) failed", e_failed)
 
+print("\nLoading 'plot_freq.py'...")
 subprocess.run('./param/aspifile11/plot/plot_freq.py', check=True)

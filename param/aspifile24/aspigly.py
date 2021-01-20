797b8844bc80fd3c6ae5cdfa0e31976e5af38f8d
TLRKiliann <philogenie@protonmail.com>
2021-01-20 03:00:50 +0100

    aspifile 24 ok

--- param/aspifile24/aspigly.py
+++ param/aspifile24/aspigly.py
@@ -52,6 +52,17 @@ print("\nThat seems correct!\n")
 with open('./param/aspifile24/data_gly.json', 'a+') as datafile:
     json.dump(data_list2, datafile, indent=4)
 
-print("\nDownloading 'plot_prog.py'...")
+try:
+    proc = subprocess.run(["scp", "./param/paramdata24.txt",
+        "pi@192.168.18.12:~/tt_doc/doc_txt24/paramdata24.txt"],
+        stderr=subprocess.PIPE)
+    print("Result SCP transfert : %s" % repr(proc.stderr))
+    secproc = subprocess.run(["scp", "./param/aspifile24/gly.json",
+        "pi@192.168.18.12:~/tt_doc/doc_txt24/gly.json"],
+        stderr=subprocess.PIPE)
+    print("Result SCP transfert : %s" % repr(secproc.stderr))
+except (OSError, FileNotFoundError) as e_failed:
+    print("+ SCP transfert (upload) failed", e_failed)
 
+print("\nLoading 'plot_gly.py'...")
 subprocess.run('./param/aspifile24/plot/plot_gly.py', check=True)

504f6fc55cee37c8382aab96373cca2c67b15023
TLRKiliann <philogenie@protonmail.com>
2021-01-25 16:14:09 +0100

    shootransfert in progress

--- shootransfert.py
+++ shootransfert.py
@@ -21,7 +21,7 @@ def loaderfile():
                 './newpatient/entryfile2.txt')
             print("+ entryfile 2 copied")
     except FileNotFoundError as nf_file2:
-        print("Not found", nf_file2) --ok--
+        print("Not found", nf_file2)
 
     try:
         if os.path.exists('./newpatient/entryfile3.txt'):
@@ -46,3 +46,43 @@ def loaderfile():
             print("+ entryfile 5 copied")
     except FileNotFoundError as nf_file5:
         print("Not found", nf_file5)
+
+    try:
+        if os.path.exists('./newpatient/entryfile6.txt'):
+            shutil.copy('./Backup/Files6/Backup_patient6.txt',
+                './newpatient/entryfile6.txt')
+            print("+ entryfile 6 copied")
+    except FileNotFoundError as nf_file6:
+        print("Not found", nf_file6)
+
+    try:
+        if os.path.exists('./newpatient/entryfile7.txt'):
+            shutil.copy('./Backup/Files7/Backup_patient7.txt',
+                './newpatient/entryfile7.txt')
+            print("+ entryfile 7 copied")
+    except FileNotFoundError as nf_file7:
+        print("Not found", nf_file7)
+
+    try:
+        if os.path.exists('./newpatient/entryfile8.txt'):
+            shutil.copy('./Backup/Files8/Backup_patient8.txt',
+                './newpatient/entryfile8.txt')
+            print("+ entryfile 8 copied")
+    except FileNotFoundError as nf_file8:
+        print("Not found", nf_file8)
+
+    try:
+        if os.path.exists('./newpatient/entryfile9.txt'):
+            shutil.copy('./Backup/Files9/Backup_patient9.txt',
+                './newpatient/entryfile9.txt')
+            print("+ entryfile 9 copied")
+    except FileNotFoundError as nf_file9:
+        print("Not found", nf_file9)
+
+    try:
+        if os.path.exists('./newpatient/entryfile10.txt'):
+            shutil.copy('./Backup/Files10/Backup_patient10.txt',
+                './newpatient/entryfile10.txt')
+            print("+ entryfile 10 copied")
+    except FileNotFoundError as nf_file10:
+        print("Not found", nf_file10)

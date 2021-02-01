e300d2c16c4104d3fac28f1aefa37b93fd56ba0c
TLRKiliann <philogenie@protonmail.com>
2021-02-01 19:28:34 +0100

    to improve GUI python3-tkinter with tk

--- vmed/vm_patient15.py
+++ vmed/vm_patient15.py
@@ -10,59 +10,59 @@ import subprocess
 
 
 # La ScrollBar en class! Préparation pour l'application.
-class ScrollCanvas(Frame):
+class ScrollCanvas(tk.Frame):
     def __init__(self, boss=None):
-        Frame.__init__(self, borderwidth=borderwidth, relief=relief)
-        self.can = Canvas(self, width=width, height=height, bd=bd, bg=bg,
+        tk.Frame.__init__(self, borderwidth=borderwidth, relief=relief)
+        self.can = tk.Canvas(self, width=width, height=height, bd=bd, bg=bg,
             relief=relief)
-        self.frame = Frame(self.can)
+        self.frame = tk.Frame(self.can)
 
-        self.vsb = Scrollbar(self, orient=VERTICAL, command=self.can.yview)
+        self.vsb = tk.Scrollbar(self, orient=VERTICAL, command=self.can.yview)
         self.can.configure(yscrollcommand=self.vsb.set)
         
-        self.vsb.pack(side=RIGHT, fill=Y)
-        self.can.pack(side=LEFT, fill=BOTH, expand=YES)
-        self.can.create_window((4, 4), window=self.frame, anchor=NW,
+        self.vsb.pack(side=tk.RIGHT, fill=Y)
+        self.can.pack(side=tk.LEFT, fill=BOTH, expand=YES)
+        self.can.create_window((4, 4), window=self.frame, anchor=tk.NW,
             tags="self.frame")
         self.frame.bind("<Configure>", self.onFrameConfigure)
 
 # Class de la barre des menus
-class MenuBar(Frame):
+class MenuBar(tk.Frame):
     """Barre menu déroulant"""
     def __init__(self, boss=None):
-        Frame.__init__(self, borderwidth=5, bg='RoyalBlue3', padx=0)
-        But2 = Button(self, text ="Close", fg='cyan', bg='RoyalBlue3',
+        tk.Frame.__init__(self, borderwidth=5, bg='RoyalBlue3', padx=0)
+        But2 = tk.Button(self, text ="Close", fg='cyan', bg='RoyalBlue3',
             relief=GROOVE, activebackground='pale turquoise',
-            command=boss.quit).pack(side=LEFT, padx=3)
+            command=boss.quit).pack(side=tk.LEFT, padx=3)
 
 # Application principale
-class Application(Frame):
+class Application(tk.Frame):
     def __init__(self, boss=None):
-        Frame.__init__(self)
+        tk.Frame.__init__(self)
         self.master.title('Medical Visit')
         mBar = MenuBar(self)
         mBar.pack(side=TOP, fill=X, expand=1)
         # ScrollCanvas limite de la zone à parcourir avec la barre
-        self.can = Canvas(self, width=600, height=400, bg='DodgerBlue2')
-        self.frame = Frame(self.can)
-        self.vsb = Scrollbar(self, orient=VERTICAL, command=self.can.yview)
+        self.can = tk.Canvas(self, width=600, height=400, bg='DodgerBlue2')
+        self.frame = tk.Frame(self.can)
+        self.vsb = tk.Scrollbar(self, orient=VERTICAL, command=self.can.yview)
         self.can.configure(yscrollcommand=self.vsb.set)
-        self.vsb.pack(side=RIGHT, fill=Y)
-        self.can.pack(side=LEFT, fill=BOTH, expand=YES)
-        self.can.create_window((4,4), window=self.frame, anchor=NW,
+        self.vsb.pack(side=tk.RIGHT, fill=Y)
+        self.can.pack(side=tk.LEFT, fill=BOTH, expand=YES)
+        self.can.create_window((4,4), window=self.frame, anchor=tk.NW,
             tags="self.frame")
         # Insertion du texte
-        self.can.create_text(300, 150, anchor=CENTER,
+        self.can.create_text(300, 150, anchor=tk.CENTER,
             text="Medical Visit", font=('Times New Roman', 28),
             fill='aquamarine')
-        self.can.create_text(590, 375, anchor=NE, text="ko@l@tr33",
+        self.can.create_text(590, 375, anchor=tk.NE, text="ko@l@tr33",
             font=('Times', 12), fill='white') 
-        self.can.pack(side=LEFT, fill=BOTH, expand=1)
+        self.can.pack(side=tk.LEFT, fill=BOTH, expand=1)
         # Configuration de la Scrollbar sur le Frame
         self.frame.bind("<Configure>", self.onFrameConfigure)
         # Button to add
         self.x2, self.y2 = 200, 250
-        self.b2 = Button(self.can, width=10, font=16, bd=3,
+        self.b2 = tk.Button(self.can, width=10, font=16, bd=3,
             bg='RoyalBlue2', fg='white',
             activebackground='pale turquoise',
             highlightbackground='cyan',
@@ -71,7 +71,7 @@ class Application(Frame):
             self.y2, window=self.b2)
         # Button to read
         self.x3, self.y3 = 400, 250
-        self.b3 = Button(self.can, width=10, font=16, bd=3,
+        self.b3 = tk.Button(self.can, width=10, font=16, bd=3,
             bg='RoyalBlue2', fg='white',
             activebackground='pale turquoise',
             highlightbackground='cyan',

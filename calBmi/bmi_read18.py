67b023b69e1b2b644918e2885c6ec8f1366e8abe
TLRKiliann <philogenie@protonmail.com>
2021-02-02 18:39:21 +0100

    up and download at work

--- calBmi/bmi_read18.py
+++ calBmi/bmi_read18.py
@@ -31,14 +31,14 @@ with open('./newpatient/entryfile18.txt', 'r') as filename:
     line_b = filename.readline()
     line_c = filename.readline()
 
-text_name = StringVar()
+text_name = tk.StringVar()
 text_name.set(line_a[:-1])
-Entryname = Entry(fen, textvariable=text_name)
-Entryname.pack(in_=top, side=LEFT, pady=20)
+Entryname = tk.Entry(fen, textvariable=text_name)
+Entryname.pack(in_=top, side=tk.LEFT, pady=20)
 
-text_all = StringVar()
+text_all = tk.StringVar()
 text_all.set(line_c[:-1])
-Entryall = Entry(fen, textvariable=text_all, width=60)
+Entryall = tk.Entry(fen, textvariable=text_all, width=60)
 Entryall.pack(padx=10, pady=5)
 
 def importationFile(fichier, encodage="Utf-8"):
@@ -46,19 +46,19 @@ def importationFile(fichier, encodage="Utf-8"):
     content = file.readlines()
     file.close()
     for li in content:
-        textBox.insert(END, li)
+        textBox.insert(tk.END, li)
 
 def msgBox():
     messagebox.showinfo('Info', 'File bmi18.txt does not exist')
 
-textBox = Text(fen, height=15, width=60, font=18)
+textBox = tk.Text(fen, height=15, width=60, font=18)
 textBox.pack(padx=30, pady=30)
 
-buttonClose = Button(fen, text="Quit", width=10, bd=3,
+buttonClose = tk.Button(fen, text="Quit", width=10, bd=3,
     fg='cyan', bg='RoyalBlue3', activebackground='pale turquoise', 
     activeforeground='navy', highlightbackground='cyan',
     command=quit)
-buttonClose.pack(side='right', padx=10, pady=10)
+buttonClose.pack(side=tk.RIGHT, padx=10, pady=10)
 
 try:
     importationFile('./calBmi/bmi18.txt', encodage="Utf-8")

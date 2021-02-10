8e1ea481521faa62974c5e96b1454f2f86ea67f8
TLRKiliann <philogenie@protonmail.com>
2021-02-10 22:11:33 +0100

    agenda_xtnd and main if rdv modified to upload ok

--- patient_agenda/events4/doc_events/fix_agenda/extend_agenda.py
+++ patient_agenda/events4/doc_events/fix_agenda/extend_agenda.py
@@ -152,7 +152,7 @@ def retrieve_input():
         print("+ './Backup/Files4' uploaded !")
     else:
         print("+ No file to upload !")
-        messagebox.showerror("Error", "./Backup/Files4 not uploaded")
+        messagebox.showerror("Error", "./Backup/Files4 not uploaded...")
 
 def messFromSafeButt():
     """

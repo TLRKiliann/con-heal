71e1cc9f868d2b4c55cb2d00be3b41a2405db09b
TLRKiliann <philogenie@protonmail.com>
2021-02-28 10:29:28 +0100

    to delete files from dmst and auxiliary added

--- deletepatient/del_patient10.py
+++ deletepatient/del_patient10.py
@@ -259,6 +259,97 @@ def delFuncFile10():
         messagebox.showerror("Error", "No entryfile10.txt to upload...")
 
     try:
+        if os.path.getsize('./auxequip/doc_equip10/auxiliary1.txt'):
+            os.remove('./auxequip/doc_equip10/auxiliary1.txt')
+            print("+ File auxiliary1.txt deleted")
+    except FileNotFoundError as filefunc20:
+        print("+ File auxiliary1.txt does not exist", filefunc20)
+
+    try:
+        if os.path.getsize('./contact/conpact10/finalfile1.txt'):
+            os.remove('./contact/conpact10/finalfile1.txt')
+            print("+ File finalfile1.txt deleted")
+    except FileNotFoundError as filefunc21:
+        print("+ File finalfile1.txt does not exist", filefunc21)
+
+    try:
+        if os.path.getsize('./contact/conpact10/finaldoc1.txt'):
+            os.remove('./contact/conpact10/finaldoc1.txt')
+            print("+ File finaldoc1.txt deleted")
+    except FileNotFoundError as filefunc22:
+        print("+ File finaldoc1.txt does not exist", filefunc22)
+
+    try:
+        if os.path.getsize('./contact/conpact10/finaldoc2.txt'):
+            os.remove('./contact/conpact10/finaldoc2.txt')
+            print("+ File finaldoc2.txt deleted")
+    except FileNotFoundError as filefunc23:
+        print("+ File finaldoc2.txt does not exist", filefunc23)
+
+    try:
+        if os.path.getsize('./contact/conpact10/finaldoc3.txt'):
+            os.remove('./contact/conpact10/finaldoc3.txt')
+            print("+ File finaldoc3.txt deleted")
+    except FileNotFoundError as filefunc24:
+        print("+ File finaldoc3.txt does not exist", filefunc24)
+
+    try:
+        if os.path.getsize('./contact/conpact10/finalfam1.txt'):
+            os.remove('./contact/conpact10/finalfam1.txt')
+            print("+ File finalfam1.txt deleted")
+    except FileNotFoundError as filefunc25:
+        print("+ File finalfam1.txt does not exist", filefunc25)
+
+    try:
+        if os.path.getsize('./contact/conpact10/finalhcs1.txt'):
+            os.remove('./contact/conpact10/finalhcs1.txt')
+            print("+ File finalhcs1.txt deleted")
+    except FileNotFoundError as filefunc26:
+        print("+ File finalhcs1.txt does not exist", filefunc26)
+
+    try:
+        if os.path.getsize('./contact/conpact10/finalhcs2.txt'):
+            os.remove('./contact/conpact10/finalhcs2.txt')
+            print("+ File finalhcs2.txt deleted")
+    except FileNotFoundError as filefunc27:
+        print("+ File finalhcs2.txt does not exist", filefunc27)
+
+    try:
+        if os.path.getsize('./contact/conpact10/finalhcs3.txt'):
+            os.remove('./contact/conpact10/finalhcs3.txt')
+            print("+ File finalhcs3.txt deleted")
+    except FileNotFoundError as filefunc28:
+        print("+ File finalhcs3.txt does not exist", filefunc28)
+
+    try:
+        if os.path.getsize('./medidoc/doc_dmst10/parcours.txt'):
+            os.remove('./medidoc/doc_dmst10/parcours.txt')
+            print("+ File parcours.txt deleted")
+    except FileNotFoundError as filefunc29:
+        print("+ File parcours.txt does not exist", filefunc29)
+
+    try:
+        if os.path.getsize('./medidoc/doc_dmst10/pbm.txt'):
+            os.remove('./medidoc/doc_dmst10/pbm.txt')
+            print("+ File pbm.txt deleted")
+    except FileNotFoundError as filefunc30:
+        print("+ File pbm.txt does not exist", filefunc30)
+
+    try:
+        if os.path.getsize('./medidoc/doc_dmst10/project.txt'):
+            os.remove('./medidoc/doc_dmst10/project.txt')
+            print("+ File project.txt deleted")
+    except FileNotFoundError as filefunc31:
+        print("+ File project.txt does not exist", filefunc31)
+
+    try:
+        if os.path.getsize('./medidoc/doc_dmst10/rslt_dmst1.txt'):
+            os.remove('./medidoc/doc_dmst10/rslt_dmst1.txt')
+            print("+ File rslt_dmst1.txt deleted")
+    except FileNotFoundError as filefunc32:
+        print("+ File rslt_dmst1.txt does not exist", filefunc32)
+
+    try:
         if os.path.exists('./Backup/Files10/Backup_param10.txt'):
             print("+ Backup_param10.txt exist")
             shutil.copy('./Backup/Files10/Backup_param10.txt',

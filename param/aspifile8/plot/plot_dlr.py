ea3589b876cbeb5729a2dd17f5b9e88aeadc9feb
TLRKiliann <philogenie@protonmail.com>
2021-03-05 21:53:25 +0100

    color fig.set_facecolor() changed

--- param/aspifile8/plot/plot_dlr.py
+++ param/aspifile8/plot/plot_dlr.py
@@ -71,7 +71,7 @@ try:
     show_grid = True
     with plt.style.context('seaborn-darkgrid'):
         fig = plt.figure()
-        fig.set_facecolor("aqua")
+        fig.set_facecolor("lightsteelblue")
         lab = fig.suptitle('Pain Scale (dlr/10) by Day',
             fontsize=18)
         lab.set_color('navy')
